ece9e9f77c2a979dd68f16ca8fda1016cebf602c
Jason Cairns <jason.cairns@auckland.ac.nz>
2022-03-21 15:42:13 +1300

    living prereqs init

---
+++ docs/life-prereq.mk
@@ -0,0 +1,59 @@
+# All to have reading references and directions
+# TODO: more on blacksmithing, textile manufacture, plumbing, pottery, animal butchery & husbandry, building construction
+
+health: homeostasis hygiene
+
+homeostasis: energy core-temperature
+hygiene: bathing toiletry oral-hygiene
+
+energy: nutrition sleep
+core-temperature: clothing shelter
+bathing: bath-water bath-tub soap wash-cloth shelter hot-bath-water
+toiletry: rinse wipe toilet shelter hand-washing
+oral-hygiene: drinking-water toothpaste toothbrush floss
+
+nutrition: drinking-water food
+sleep: shelter blanket bed bed-sheets pillow
+clothing: garments footwear
+shelter: flooring walls door roof heating
+bath-water: rain-water-capture-system bath-plumbing
+bath-tub: bath-tub-construction bath-tub-maintenance
+soap: rendered-fat lye pot firepit container
+hot-bath-water: back-boiler back-boiler-plumbing 
+
+bath-tub-construction: wooden-strips tub-bands nails hammer cork woodworking-chisel saw bath-plumbing
+bath-tub-maintenance: cleaning-brush soap water bucket squeegee mop-cloth
+drinking-water: water-spring-well drinking-cups drinking-water-plumbing water-purification
+food: base-and-essence-main side-dishes snacks
+blanket: wool-yarn loom
+high-direct-heat: stove firepit
+high-indirect-heat: oven
+rendered-fat: pot water firepit container
+hammer: hammer-handle wedge  hammer-head
+woodworking-chisel: woodworking-chisel-head woodworking-chisel-handle sharpening
+
+hammer-handle woodworking-chisel-handle: wood gouge chisel hammer saw
+base-and-essence-main: bread-and-cooked-meat bread-and-cooked-egg pasta-meat-sauce crackers-and-cheese 
+stove: masonry-blocks mortar fire sweep pan
+firepit: masonry-blocks fire sweep pan
+sharpening: grind-wheel sharpening-stones honing-strop 
+
+cooked-meat-and-bread: cooked-meat bread salt
+
+salt: seawater-brine briquetage saltern-fire
+cooked-meat: grilled-meat baked-meat
+
+grilled-meat: pan stove oil raw-meat
+baked-meat: tray oven oil raw-meat
+bread: flour water salt yeast oven proving-basket bread-mixing-bowl dutch oven
+	@forkish2012fwsy
+
+fire: tinderbox kindling firewood
+
+flour: wheat mill flour-container
+yeast: flour water levain-container
+
+roof: timber shingles walls
+
+walls: timber infill foundations
+
